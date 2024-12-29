46253d1629be8a8c834831e6573617b74085721b
bulle21 <ohoel2@yahoo.fr>
2024-12-29 14:13:29 +0100

    maj du macbookpro

--- 1-benchmark.c
+++ 1-benchmark.c
@@ -7,7 +7,7 @@
 #include <time.h>
 
 
-#define ITERATIONS 100000000
+#define ITERATIONS 1000000000
 
 double get_time_elapsed(struct timespec start, struct timespec end) {
     return (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
